f5b51c3a62fd260760996a45b57cd0cd1b8e581a
bgaheer <baljeetgaheer@gmail.com>
2017-11-03 16:11:47 +0530

    changes

--- app/src/main/java/in/learntech/rights/Managers/CompanyUserManager.java
+++ app/src/main/java/in/learntech/rights/Managers/CompanyUserManager.java
@@ -1,8 +1,58 @@
 package in.learntech.rights.Managers;
 
+import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.learntech.rights.BusinessObjects.CompanyUser;
+import in.learntech.rights.DataStoreMgr.CompanyUserDataStore;
+import in.learntech.rights.utils.PreferencesUtil;
+
 /**
  * Created by baljeetgaheer on 31/10/17.
  */
 
 public class CompanyUserManager {
+    private static CompanyUserManager sInstance;
+    private static CompanyUserDataStore companyUsersDataStore;
+    private static PreferencesUtil mPreferencesUtil;
+
+    public static synchronized CompanyUserManager getInstance(Context context) {
+        if (sInstance == null){
+            sInstance = new CompanyUserManager();
+            companyUsersDataStore = new CompanyUserDataStore(context);
+            mPreferencesUtil = PreferencesUtil.getInstance(context);
+        }
+        return sInstance;
+    }
+
+    public void saveUsersFromResponse(JSONObject response)throws Exception {
+        JSONArray jsonArray = response.getJSONArray("users");
+        companyUsersDataStore.deleteAll();
+        int companySeq = mPreferencesUtil.getLoggedInUserCompanySeq();
+        for (int i=0;i < jsonArray.length() ; i++) {
+            JSONObject userJson = jsonArray.getJSONObject(i);
+            int seq = userJson.getInt("seq");
+            String type = userJson.getString("type");
+            String userName = userJson.getString("username");
+            String image = userJson.getString("image");
+            CompanyUser companyUser = new CompanyUser();
+            companyUser.setSeq(seq);
+            companyUser.setUserName(userName);
+            companyUser.setType(type);
+            companyUser.setImageName(image);
+            companyUser.setCompanySeq(companySeq);
+            companyUsersDataStore.save(companyUser);
+        }
+    }
+
+    public ArrayList<CompanyUser> getCompanyUsersForLoggedInUser(){
+        return companyUsersDataStore.getCompanyUsersByCompanySeq(
+                mPreferencesUtil.getLoggedInUserCompanySeq());
+    }
+
 }
