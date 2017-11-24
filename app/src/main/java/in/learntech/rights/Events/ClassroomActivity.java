83607314929b6eded6dab70e8dfdf690201b48db
bgaheer <baljeetgaheer@gmail.com>
2017-11-24 14:23:35 +0530

    Event changes

---
+++ app/src/main/java/in/learntech/rights/Events/ClassroomActivity.java
@@ -0,0 +1,15 @@
+package in.learntech.rights.Events;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.learntech.rights.R;
+
+public class ClassroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classroom);
+    }
+}
---
+++ app/src/main/java/in/learntech/rights/Events/EventAdapter.java
@@ -0,0 +1,8 @@
+package in.learntech.rights.Events;
+
+/**
+ * Created by baljeetgaheer on 23/11/17.
+ */
+
+public class EventAdapter {
+}
