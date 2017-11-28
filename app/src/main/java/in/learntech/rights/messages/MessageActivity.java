b6aa94573299e0537da80c5fac139911c4b16284
bgaheer <baljeetgaheer@gmail.com>
2017-11-28 10:58:21 +0530

    changes

--- app/src/main/java/in/learntech/rights/messages/MessageActivity.java
+++ app/src/main/java/in/learntech/rights/messages/MessageActivity.java
@@ -1,20 +1,15 @@
 package in.learntech.rights.messages;
 
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+
 import com.tubb.smrv.SwipeMenuRecyclerView;
 
 import org.json.JSONArray;
